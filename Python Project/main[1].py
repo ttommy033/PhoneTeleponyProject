5f443382973a1ef35acd52f03d825165218fb076
ttommy033 <82055883+ttommy033@users.noreply.github.com>
2021-04-06 14:05:43 -0500

    Add files via upload

---
+++ Python Project/main[1].py
@@ -0,0 +1,21 @@
+##Author:       Thomas Reidy, Joeseph Kuzera
+##Class:        CPSC4600 Programming Language
+##Date:         December 18th, 2020
+##File:         Main File
+import Building as bld
+from copy import deepcopy
+
+def main():
+  building_main_strat =  bld.Building()
+  building_bad_strat = deepcopy(building_main_strat)
+
+  print("This is the total number of steps the elevator must make in the best case: ", building_main_strat.output())
+  building_bad_strat.strategy = 1
+  print("This is the total number of steps the elevator must make in the worst case: ", building_bad_strat.output())
+
+  
+
+if __name__ == "__main__":
+  main()
+
+
